3ca2fcc443d3d77cbb71bde4ce617bf84c080a09
Sergei <sergeyevrfr@gmail.com>
2024-04-29 11:53:47 +0400

    refactor, added byTagAndText method

--- src/test/java/HoverTest.java
+++ src/test/java/HoverTest.java
@@ -2,7 +2,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.WebDriverConditions.url;
@@ -25,10 +25,7 @@ public class HoverTest
 
         open("https://github.com");
 
-        $$("button.HeaderMenu-link.border-0.width-full.width-lg-auto." +
-                "px-0.px-lg-2.py-3.py-lg-2.no-wrap.d-flex.flex-items-center." +
-                "flex-justify-between.js-details-target")
-                .findBy(text("Solutions")).hover();
+        $(byTagAndText("button","Solutions")).hover();
         $("[href='/enterprise']").click();
         webdriver().shouldHave(url("https://github.com/enterprise"));
 
